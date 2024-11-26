8da6f789fdbebaf5d9b56e3c80efec28eb63b05f
Jordi <jordiigarciatorres@gmail.com>
2024-11-26 18:03:29 +0100

    first commit

---
+++ src/test/java/com/jea/cashpals/CashPalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jea.cashpals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashPalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
